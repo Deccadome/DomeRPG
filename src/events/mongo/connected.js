3538a59f3a2f62651a09eba5bd8a1ab6b3d9833f
Deccadome <deccadome@gmail.com>
2022-09-26 21:53:42 -0400

    Added timestamps to database connect events

--- src/events/mongo/connected.js
+++ src/events/mongo/connected.js
@@ -3,6 +3,6 @@ const chalk = require("chalk");
 module.exports = {
     name: "connected",
     execute() {
-        console.log(chalk.green("[Database Status]: Connected."));
+        console.log(chalk.green(`[Database Status]${(new Date(Date.now())).toString().substr(4,20)}: Connected.`));
     }
 }
